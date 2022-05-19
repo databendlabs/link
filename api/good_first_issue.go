27469b48f126ad3fad85c3bce1868b7183cdb43c
Xuanwo <github@xuanwo.io>
2022-05-19 10:58:11 +0800

    Call ListIssuesByRepo instead
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- api/good_first_issue.go
+++ api/good_first_issue.go
@@ -2,9 +2,10 @@ package api
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
+	"sync"
 
 	"github.com/google/go-github/v44/github"
 )
@@ -14,11 +15,27 @@ func GoodFirstIssue(w http.ResponseWriter, r *http.Request) {
 
 	ctx := context.Background()
 
-	issues, _, err := client.Issues.ListByOrg(ctx, "datafuselabs", &github.IssueListOptions{Labels: []string{"good first issue"}})
-	if err != nil {
-		fmt.Fprintf(w, "ListByOrg: %s", err)
-		return
+	repos := []string{"databend", "openraft", "opendal"}
+
+	wg := &sync.WaitGroup{}
+	var issues []*github.Issue
+	lock := sync.Mutex{}
+
+	for _, v := range repos {
+		wg.Add(1)
+
+		go func(repo string) {
+			is, _, err := client.Issues.ListByRepo(ctx, "datafuselabs", repo, &github.IssueListByRepoOptions{Labels: []string{"good first issue"}})
+			if err != nil {
+				log.Fatalf("ListByOrg: %s", err)
+			}
+
+			lock.Lock()
+			defer lock.Unlock()
+			issues = append(issues, is...)
+		}(v)
 	}
+	wg.Wait()
 
 	index := rand.Intn(len(issues))
 	url := *issues[index].URL
